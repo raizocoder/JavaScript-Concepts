e68887ec64ff724dfaa5d9148a9e016d90d2c575
rohitchamoli <ronykumar4@gmail.com>
2024-09-18 16:20:34 +0530

    start js file

---
+++ 0var-let-const/script.js
@@ -0,0 +1,9 @@
+// Difference between var let and const
+//(1) Declaration and Assignment
+//(2) Redecoration and Reassignment
+//(3) Scopes
+//(4) Hoisting
+//(5) Hoisting
+//(6) Store
+
+//(1) Declaration and Assignment------------------>
