9dde11d7e31aca636cf91ed37b0fdcdaf6a6566c
rohitchamoli <ronykumar4@gmail.com>
2024-09-25 15:14:04 +0530

    second case

--- 0var-let-const/script.js
+++ 0var-let-const/script.js
@@ -8,16 +8,39 @@
 
 //(1) Declaration and Assignment in separate lines------------------>
 
-var a;
-a = 1;
-console.log(a); // no error
+// var a;
+// a = 1;
+// console.log(a); // no error
 
-let b ;
-b = 2;
-console.log(b); // no error
+// let b;
+// b = 2;
+// console.log(b); // no error
 
-const c = 3; // in const it is compulsory for  declaration and assignment in on line;
-
-
+// const c = 3; // in const it is compulsory for  declaration and assignment in on line;
 
 //(2)  Redecoration and Reassignment with Same name----------------->
+
+// var x = 1;
+// var x = 2;
+
+// var a = 1;
+// a = 2;
+// var has feature declare and assign two times with same or different values both are correct but takes the latest values.
+
+// let y = 1;
+// y = 3; no error values changes and takes its latest values
+
+// let y = 1;
+// let y = 2;
+//  this give error because let has feature only to assign again only value not to declare with same name;
+
+
+
+// const a = 1;
+// a = 2;
+//error comes
+
+// const a = 1;
+// const a = 3;
+// error comes
+// const does not have feature to reassign and redeclaration
