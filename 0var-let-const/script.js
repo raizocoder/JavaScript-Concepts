1ecd642cf2772c4d74fc84721e1affc4174802ec
rohitchamoli <ronykumar4@gmail.com>
2024-09-25 07:05:21 +0530

    first case

--- 0var-let-const/script.js
+++ 0var-let-const/script.js
@@ -1,9 +1,23 @@
 // Difference between var let and const
-//(1) Declaration and Assignment
+//(1) Declaration and Assignment in separate lines
 //(2) Redecoration and Reassignment
 //(3) Scopes
 //(4) Hoisting
 //(5) Hoisting
 //(6) Store
 
-//(1) Declaration and Assignment------------------>
+//(1) Declaration and Assignment in separate lines------------------>
+
+var a;
+a = 1;
+console.log(a); // no error
+
+let b ;
+b = 2;
+console.log(b); // no error
+
+const c = 3; // in const it is compulsory for  declaration and assignment in on line;
+
+
+
+//(2)  Redecoration and Reassignment with Same name----------------->
